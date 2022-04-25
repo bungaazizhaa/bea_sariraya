e87ff1c500589abf1ead83e30769895715f9a903
Alvin Alvrahesta <alvin.alvrahesta@gmail.com>
2022-04-25 21:48:15 +0700

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
+use App\Models\Univ;
 use App\Models\User;
-use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Prodi;
 use Illuminate\Support\Facades\Hash;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Redirect;
+use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
@@ -41,6 +46,13 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
+    public function showRegistrationForm()
+    {
+        $getAllProdi = Prodi::all();
+        $getAllUniv = Univ::all();
+        return view('Auth.register', compact('getAllProdi', 'getAllUniv'));
+    }
+
     /**
      * Get a validator for an incoming registration request.
      *
@@ -50,10 +62,20 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'regex:/^[a-z A-Z]+$/u', 'max:255'],
+            'nim' => ['required', 'string', 'max:255'],
+            'univ_id' => ['required'],
+            'prodi_id' => ['required'],
+            // 'univ_id_manual' => ['string', 'regex:/^[a-z A-Z]+$/u', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
+
+        if (isset($data['univ_id_manual'])) {
+            return Validator::make($data, [
+                'univ_id_manual' => ['string', 'regex:/^[a-z A-Z]+$/u', 'max:255'],
+            ]);
+        }
     }
 
     /**
@@ -64,10 +86,41 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        if ($data['univ_id'] == "other") {
+            $getUniv = Univ::where('nama_universitas', '=', $data['univ_id_manual'])->first();
+
+            if ($getUniv) {
+                return User::create([
+                    'name' => $data['name'],
+                    'nim' => $data['nim'],
+                    'univ_id' => $getUniv->id,
+                    'prodi_id' => $data['prodi_id'],
+                    'email' => $data['email'],
+                    'password' => Hash::make($data['password']),
+                ]);
+            } else {
+                Univ::create([
+                    'nama_universitas' => $data['univ_id_manual'],
+                ]);
+                $getUniv = Univ::where('nama_universitas', '=', $data['univ_id_manual'])->first();
+                return User::create([
+                    'name' => $data['name'],
+                    'nim' => $data['nim'],
+                    'univ_id' => $getUniv->id,
+                    'prodi_id' => $data['prodi_id'],
+                    'email' => $data['email'],
+                    'password' => Hash::make($data['password']),
+                ]);
+            }
+        } else {
+            return User::create([
+                'name' => $data['name'],
+                'nim' => $data['nim'],
+                'univ_id' => $data['univ_id'],
+                'prodi_id' => $data['prodi_id'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
+        }
     }
 }
