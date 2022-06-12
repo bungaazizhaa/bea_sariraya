4bd126a5664dae5933cf01ee2bca3c0c82287623
Bunga Azizha <bungaazizha33@gmail.com>
2022-06-12 21:28:17 +0700

    Perubahan bg pengumuman akhir

--- resources/views/view-mahasiswa/penugasan/p-pengumumanlolos.blade.php
+++ resources/views/view-mahasiswa/penugasan/p-pengumumanlolos.blade.php
@@ -9,17 +9,13 @@
 
 
     {{-- Bootstrap 4 --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
     </script>
 
     <!-- CSS -->
@@ -35,88 +31,50 @@
 </head>
 
 <body class=" d-flex flex-column" style="min-height: calc(100vh - 60px);">
+    <div class="banner-regist" style="background-image: url('{{ asset('assets/images/berhasil.jpg') }}')"> </div>
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container d-flex justify-content-between">
-            <a class="logoo" href="#">
-                <img src="{{ asset('assets/images/logo.png') }}" alt="" width="80px">
-            </a>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto my-2 my-md-0">
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto ml-md-0 test">
-
-                        <!-- Authentication Links -->
-                        @auth
-                            <li class="nav-item mr-0 mr-md-4 pr-md-0 test"><a class="nav-link  pr-3 pr-md-2"
-                                    href="{{ url('/my-profile') }}">Profil</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::user()->name }}
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                        class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endauth
-                    </ul>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- Akhir Navbar -->
-    @include('sweetalert::alert')
     <div class="container">
-        <div class="text-center">
-            <h1 class="mt-4 test"> <b>BEASISWA SARIRAYA JAPAN 2022</b> </h1>
-
-            <h1 class="mt-3 h2">Pengumuman Akhir</h1>
-
-            <div class="alert alert-success mt-4 teksalert" role="alert">
-                <strong>SELAMAT ! ANDA DITERIMA PADA PROGRAM <br>
-                    BEASISWA SARIRAYA JAPAN 2022</strong>
-            </div>
-
-            <p class="h5 pt-2 pt-md-4">Berikut adalah link untuk masuk ke Grup WhatsApp :</p>
-            <a href="{{ url($getPeriodeAktif->group_wa) }}" class="btn btn-success mb-3 mt-4">Gabung ke Grup
-                WhatsApp</a>
-            <p>Atau klik link berikut <span><a
-                        href="{{ $getPeriodeAktif->group_wa }}">{{ $getPeriodeAktif->group_wa }}</a></span></p>
-            <br>
-            <p>Jika ada kendala, silahkan hubungi Admin.</p>
-            @auth
-                <a href="{{ url('/my-profile') }}" class="btn btn-outline-secondary mb-4 mt-2"><i
-                        class="fa-solid fa-angle-left"></i>
-                    Kembali ke
-                    Profil Anda</a>
-            @endauth
+        <div class="logo2">
+            <img src="{{ asset('assets/images/logo.png') }}" alt="">
         </div>
     </div>
+    <div class="container">
 
-    <!-- Footer -->
-    <footer class="pt-3 pt-3 pb-1 border-top bg-dark mt-auto">
-        <div class="text-center bg-dark text-white">
-            <p>&copy; Sariraya 2022</p>
+        {{-- <img src="{{ asset('assets/images/berhasil.jpg') }}" alt=""> --}}
+        @include('sweetalert::alert')
+        <div class="container">
+            <div class="text-center col-12 pl-lg-0 col-lg-12 text-lg-left text-center my-auto">
+                <center>
+                    <div class="pengumuman">
+                        <h1 class="mt-4 test"> <b>BEASISWA SARIRAYA JAPAN 2022</b> </h1>
+
+                        <h1 class="mt-3 h2">Pengumuman Akhir</h1>
+
+                        <div class="alert alert-success mt-4 teksalert" role="alert">
+                            <strong>SELAMAT ! ANDA DITERIMA PADA PROGRAM <br>
+                                BEASISWA SARIRAYA JAPAN 2022</strong>
+                        </div>
+
+                        <p class="h5 pt-2 pt-md-2">Berikut adalah link untuk masuk ke Grup WhatsApp :</p>
+                        <a href="{{ url($getPeriodeAktif->group_wa) }}" class="btn btn-success mb-3 mt-2">Gabung ke Grup
+                            WhatsApp</a>
+                        <p>Atau klik link berikut <span><a href="{{ $getPeriodeAktif->group_wa }}">{{ $getPeriodeAktif->group_wa }}</a></span></p>
+                        <br>
+                        <p>Jika ada kendala, silahkan hubungi Admin.</p>
+                        @auth
+                        <a href="{{ url('/my-profile') }}" class="btn btn-outline-light mb-4 mt-2"><i class="fa-solid fa-angle-left"></i>
+                            Kembali ke
+                            Profil Anda</a>
+                        @endauth
+                    </div>
+                </center>
+            </div>
+            <div class="logo">
+                <img src="{{ asset('assets/images/awardee.png') }}" alt="">
+            </div>
         </div>
-    </footer>
+
+
 </body>
 
 </html>
