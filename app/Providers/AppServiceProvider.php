0604f9bf73ddfc692f5f167cd3907e2b796dddbf
Bunga Azizha <bungaazizha33@gmail.com>
2022-06-21 20:07:06 +0700

    tes deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        if(env('APP_ENV') !=='local'){
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
+        }
     }
 }
